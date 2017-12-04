9624d37749f9742b9e3039eb0273e4fedb14c1df
Mike Roseleip <mroseleip@comcast.net>
2017-12-03 19:04:33 -0800

    basic routing

---
+++ ReactDemo3/ReactDemo3/ClientApp/components/RefPage.tsx
@@ -0,0 +1,61 @@
+﻿import * as React from 'react';
+import { RouteComponentProps } from 'react-router';
+import 'isomorphic-fetch';
+
+interface OperationExampleState {
+    operation: Operation;
+    loading: boolean;
+}
+
+interface Operation {
+    name: string;
+    summary: string;
+    http_method: string;
+    operation_id: string;
+}
+
+export class FetchOperation extends React.Component<RouteComponentProps<{}>, OperationExampleState> {
+
+    constructor() {
+        super();
+        this.state = { operation: { summary: "", operation_id: "", name: "", http_method: "" }, loading: true};
+
+        var opPart = "getEnvleope"; // url.split('/')[1];
+        var refPageUrl = 'api/RefPage/ReferencePath/' + opPart;
+        fetch(refPageUrl)
+            .then(response => response.json() as Promise<Operation>)
+            .then(data => {
+                this.setState({ operation: data, loading: false });
+            });
+
+    }
+
+    public render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : FetchOperation.renderRefPage(this.state.operation, this.props.match.url);
+
+        return <div>
+            <h1>Reference page</h1>
+            < p > This component demonstrates Rosey getting barked at by Zuzu the pest.</p>
+            {contents}
+        </div>;
+    }
+
+    private static renderRefPage(operation: Operation, op_id: string) {
+        return <div className='refpage' >
+            <div>Routed OpId HERE </div>
+            <div>{op_id}</div>
+            <div>Name</div>
+            <div>{operation.name}</div>
+            <div>Http Method</div>
+            <div>{operation.http_method}</div>
+            <div>Summary</div>
+            <div>{operation.summary}</div>
+            <div>Operation Id</div>
+            <div>{operation.operation_id}</div>
+        </div>;
+    }
+}
+
+
---
+++ ReactDemo3/ReactDemo3/Controllers/RefPageController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactDemo3.Controllers
+{
+    [Route("api/[controller]")]
+    public class RefPageController : Controller
+    {
+        [HttpGet("[action]/{operationId}")]
+        public Operation ReferencePath(string operationId)
+        {
+            Operation op = new Operation()
+            {
+                operation_id = operationId,
+                name = "Get Envelopes",
+                summary = "Gets me some envelopes",
+                http_method = "Get"
+            };
+            return op;
+        }
+    }
+
+    public class Operation
+    {
+        public string operation_id { get; set; }
+        public string name { get; set; }
+        public string summary { get; set; }
+        public string http_method { get; set; }
+    }
+}
